bfe34bc6d5cd822b5618db69698c40c090dc0ee3
Santhosh TK <tksanthosh494@gmail.com>
2022-03-20 19:33:04 +0530

    Initial commit

--- RunLengthEncoding.java
+++ RunLengthEncoding.java
@@ -16,17 +16,12 @@ public class RunLengthEncoding {
 	        for(int i=0;i<str.length()-1;i++){
 	            
 	            if(str.charAt(i) == str.charAt(i+1)){
-	                count++;
-	                
-	                
+	                count++;   
 	            }
 	            else { 
 	            	result+= str.charAt(i)+String.valueOf(count);
-	                
-	                count =1;
-	               
-	            }
-	            
+	                count =1;  
+	            }  
 	        }
 	       System.out.println( result+str.charAt(str.length()-1)+Integer.toString(count));
 	}
--- SortingArray.java
+++ SortingArray.java
@@ -17,11 +17,12 @@ public class SortingArray {
 				  if(arr[i]>arr[j]) {
 				 temp = arr[i];
 				 arr[i]= arr[j];
-				arr[j] = temp;}
+				arr[j] = temp;
+				}
 			  }
 		  }
 		  for(int i =0;i<a;i++) {
-			  System.out.print(arr[i]);
+			  System.out.print(arr[i]+" ");
 		  }
 
 	  }
